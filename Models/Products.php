a013c3b28169c011ceb3faf74053bf05fc56b664
karimTaha <karim.taha@oracle.com>
2020-10-24 17:38:22 +0200

    Create Product object, return an array of Products from Model. In Invoice view, use products array returned to calculate subtotal, taxes, and total in USD

--- Models/Products.php
+++ Models/Products.php
@@ -5,7 +5,7 @@
 
         protected function getAllProducts() {
             $conn = $this->connect();
-            $sql = "select product_id, product_name from product order by product_id";
+            $sql = "select product_name, price_usd from product order by product_id";
 
             $stmt = $conn->prepare($sql);
             $stmt->execute();
@@ -24,16 +24,23 @@
             $conn = $this->connect();
 
             $productsString = $this->getProductsString($productsArray);
-            $sql = "select product_id, product_name from product where FIND_IN_SET(product_name, \"$productsString\")  order by product_id";
+            $sql = "select product_name, price_usd from product where FIND_IN_SET(product_name, \"$productsString\")  order by product_id";
 
             $stmt = $conn->prepare($sql);
             $stmt->execute();
 
-            $stmt->bind_result($product_id, $product_name);
+            $stmt->bind_result($product_name, $price_usd);
+
+            $productsResult = array();
+            $currentProduct = null;
 
             while($stmt->fetch()) {
-                echo $product_id . ' --- ' . $product_name . "\n";
+                // echo $product_id . ' --- ' . $product_name . "\n";
+                $currentProduct = new Product($product_name, $price_usd);
+                array_push($productsResult, $currentProduct);
             }
+
+            return $productsResult;
 
             $stmt->close();
             $conn->close();
@@ -46,6 +53,24 @@
             }
             $result = substr($result, 0, strlen($result)-1);
             return $result;
+        }
+    }
+
+    class Product {
+        public $name;
+        public $priceUsd;
+        public $count;
+
+        function __construct($name, $priceUsd) {
+            $this->name = $name;
+            $this->priceUsd = $priceUsd;
+            $this->count = 0;
+        }
+        function get_name() {
+            return $this->name;
+        }
+        function get_price_usd() {
+            return $this->priceUsd;
         }
     }
 
--- Views/Invoice.php
+++ Views/Invoice.php
@@ -1,11 +1,31 @@
 <?php
-
+    /**
+     * This is a view file used to prepare and print the invoice
+     * The invoice is based on the products entered by the user
+     * The Invoice class extends Products to access the products data in the DB
+     */
     include './Models/Products.php';
 
     class Invoice extends Products {
         public function printInvoice($items) {
-            // $this->getAllProducts();
-            $this->getProducts($items);
+
+            // Call getProducts to get the products that the user entered from DB
+            $productsArray = $this->getProducts($items);
+
+            $counts = array_count_values($items);
+            $subtotal = 0;
+
+            foreach($productsArray as $product) {
+                $product->count = $counts[$product->name];
+                $subtotal += $product->count * $product->priceUsd;
+            }
+
+            $taxes = $subtotal * 0.14;
+            $total = $subtotal + $taxes;
+
+            print_r("Subtotal: $" . $subtotal . "\n");
+            print_r("Taxes: $" . $taxes . "\n");
+            print_r("Total: $" . $total . "\n");
         }
     }
 
