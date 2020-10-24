15c6b2fe04a1296e10c1d287ded9cbd7e6c5acc6
karimTaha <karim.taha@oracle.com>
2020-10-24 15:13:07 +0200

    Add database debug mode variable

--- Utils/Connector.php
+++ Utils/Connector.php
@@ -7,22 +7,24 @@
         private $password = "";
         private $dbName = "shopping_cart";
 
+        private $debug_mode = false;
+
         protected function connect(){
             
             $link =  mysqli_connect($this->host, $this->user, $this->password, $this->dbName);
 
-            if (!$link) {
+            if ($this->debug_mode && !$link) {
                 print_r("Error: Unable to connect to MySQL." . PHP_EOL);
                 print_r("Debugging errno: " . mysqli_connect_errno() . PHP_EOL);
                 print_r("Debugging error: " . mysqli_connect_error() . PHP_EOL);
                 exit;
             }
             
-            print_r("Success: A proper connection to MySQL was made! The my_db database is great." . PHP_EOL);
-            print_r("Host information: " . mysqli_get_host_info($link) . PHP_EOL);
+            if ($this->debug_mode) {
+                print_r("Success: A proper connection to MySQL was made! The my_db database is great." . PHP_EOL);
+                print_r("Host information: " . mysqli_get_host_info($link) . PHP_EOL);
+            }
 
             return $link;
         }
     }
-    
-?>
